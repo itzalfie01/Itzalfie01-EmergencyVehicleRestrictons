916969f3c044775f3d97c664110d8f185e6159dd
itzalfie01 <rohanalfie@gmail.com>
2023-02-15 19:17:55 +0000

    ...

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+author 'Itzalfie01'
+description 'Simple Script Made For Qb To Restrict Emergancy Vehicles'
+
+
+game 'gta5'
+
+client_script 'client.lua'
+
+
+--[[ 
+client {
+    'client.lua'
+}
+]]--
+ 
+
+
+
